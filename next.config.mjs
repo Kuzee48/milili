9f5b6155aa973afae0540a943290980d808e430e
Kuzee48 <rizqy.alistia1@sma.belajar.id>
2025-12-30 18:02:16 +0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'p16-novel-sign-sg.fizzopic.org' },
+      { protocol: 'https', hostname: 'p19-novel-sign-sg.fizzopic.org' },
+    ],
+  },
+};
+export default nextConfig;
